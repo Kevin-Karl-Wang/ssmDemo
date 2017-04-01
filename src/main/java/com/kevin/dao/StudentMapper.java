15cfb67a60e70cacfa5f8381dbbdc6ac6dfa0485
wangyf9 <wangyf9@asiainfo.com>
2017-04-01 15:40:30 +0800

    Initial commit

---
+++ src/main/java/com/kevin/dao/StudentMapper.java
@@ -0,0 +1,17 @@
+package com.kevin.dao;
+
+import com.kevin.model.Student;
+
+public interface StudentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Student record);
+
+    int insertSelective(Student record);
+
+    Student selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Student record);
+
+    int updateByPrimaryKey(Student record);
+}
